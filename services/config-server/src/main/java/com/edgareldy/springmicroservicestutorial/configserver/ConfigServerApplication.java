ee05974c05d8adbf69f4148ea064d46d4d662ca9
EdgarEldy <ed.eldy21@live.com>
2026-08-15 11:23:47 +0400

    docs(ConfigServerApplication): add mandatory class-level Javadoc block

--- services/config-server/src/main/java/com/edgareldy/springmicroservicestutorial/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/edgareldy/springmicroservicestutorial/configserver/ConfigServerApplication.java
@@ -3,6 +3,15 @@ package com.edgareldy.springmicroservicestutorial.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Bootstraps the Spring Cloud Config Server that centralizes each business
+ * service's application configuration from a classpath-backed config repo.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/15/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/15/26
+ * Project : spring-microservices-tutorial
+ */
 @SpringBootApplication
 public class ConfigServerApplication {
 
