4c4e0f94b98cee6f6a9272dbe5c06fb338305ca0
EdgarEldy <ed.eldy21@live.com>
2026-08-15 11:23:47 +0400

    docs(NotificationServiceApplication): add mandatory class-level Javadoc block

--- services/notification-service/src/main/java/com/edgareldy/springmicroservicestutorial/notificationservice/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/com/edgareldy/springmicroservicestutorial/notificationservice/NotificationServiceApplication.java
@@ -3,6 +3,17 @@ package com.edgareldy.springmicroservicestutorial.notificationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Bootstraps the notification service: the system's sole database-less,
+ * consumer-only microservice, responsible for every outbound e-mail and for
+ * the choreographed Saga's confirming/compensating events back to
+ * order-service.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/15/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/15/26
+ * Project : spring-microservices-tutorial
+ */
 @SpringBootApplication
 public class NotificationServiceApplication {
 
